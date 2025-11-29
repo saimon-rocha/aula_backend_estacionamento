e33b90c7a8cb6896fb857f1f551f92b64fd6982f
saimon-rocha <saimon.rocha07@gmail.com>
2025-11-28 21:05:15 -0300

    ajustado build

--- src/config/db.ts
+++ src/config/db.ts
@@ -11,6 +11,7 @@ const databaseConfig: Knex.Config = {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
+        ssl: { rejectUnauthorized: false }
     },
     pool: {
         min: 1,
